e19ea3b2cbdb59147932942458a891e250803962
andrehideki <andre.hkiw@gmail.com>
2021-10-11 17:32:43 -0300

    add: styled-components

--- src/components/Label.js
+++ src/components/Label.js
@@ -1,21 +1,40 @@
+import styled from "styled-components";
 import { FlavorImg } from "./FlavorImg";
 
 export function Label({ title, subtitle, description, weigth }) {
   return (
-    <div>
-      <header>
-        { title }
-      </header>
+    <Container>
       <main>
+        <section>{ title }</section>
         <section>{ subtitle }</section>
-        <section>
+        <aside>
           <FlavorImg />
-        </section>
+        </aside>
       </main>
       <footer>
         <div>{ description }</div>
         <div>{ weigth }</div>
       </footer>
-    </div>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  border: 5px dashed;
+
+  font-family: sans-serif;
+  
+  footer {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    background: blue;
+    color: white;
+    font-weight: bold;
+    padding: .25rem;
+  }
+`
