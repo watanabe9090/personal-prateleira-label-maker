000770ba24207307b3aef30a61b80f7671f23242
andrehideki <andre.hkiw@gmail.com>
2021-10-12 09:54:57 -0300

    add: database -> Show labels based on in memory database array

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import { Label } from "./components/Label";
 
 function App() {
+
+  const database = [
+    { 
+      title: "Special Dog",
+      subtitle: "Carne",
+      description: "Sabor Carne",
+      weigth: "1KG",
+      color: "#a64b00"
+    },
+    { 
+      title: "Special Dog",
+      subtitle: "Vegetal",
+      description: "Sabor Vegetal",
+      weigth: "1KG",
+      color: "#a64b00"
+    },
+    { 
+      title: "Magnus Petit",
+      subtitle: "Raças Pequenas",
+      description: "(Carne + Cereiais + Vegetais)",
+      weigth: "1KG",
+      color: "#a64b00"
+    }
+  ];
+
   return (
     <div>
-      <Label title="Special Dog" 
-              subtitle="Carne"
-              description="Sabor Carne"
-              weigth="1KG"
-              color="#a64b00" />
+      { database.map(({ title, subtitle, description, weigth, color }) => (
+        <Label title={ title } 
+              subtitle={ subtitle }
+              description={ description }
+              weigth={ weigth }
+              color={ color } />
+      )) }
     </div>
   );
 }
